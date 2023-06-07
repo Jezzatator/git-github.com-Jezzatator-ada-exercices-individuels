322edeb32e9a00a0dc2384ca3c3784b965cbc1c9
Jezzatator <jeremie.patot@protonmail.ch>
2023-06-07 15:22:26 +0200

    Ajout Etape 1

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -1,4 +1,11 @@
+// Etape 1
 var idName = prompt("Quel est votre nom?")
 var text = "👋 Bonjour " + idName
 
 document.body.innerHTML += '<h2> ' + text + ' </h2>'
+
+// Etape 2
+var idAge = prompt("Quel est votre âge?")
+var text = "Vous avez " + idAge + " ans"
+
+document.body.innerHTML += '<h3> ' + text + ' </h3>'
